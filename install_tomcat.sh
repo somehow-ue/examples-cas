b8485ddc4924aea993b9569c2122a12f858541d3
idtndhksha <idtndhksha@uengine.org>
2014-12-16 22:38:34 +0900

    add install tomcat script

---
+++ install_tomcat.sh
@@ -0,0 +1,5 @@
+apt-get install wget
+wget http://apache.tt.co.kr/tomcat/tomcat-7/v7.0.57/bin/apache-tomcat-7.0.57.tar.gz
+gunzip apache-tomcat-7.0.57.tar.gz
+tar xvf apache-tomcat-7.0.57.tar
+ln -s apache-tomcat-7.0.57 tomcat
